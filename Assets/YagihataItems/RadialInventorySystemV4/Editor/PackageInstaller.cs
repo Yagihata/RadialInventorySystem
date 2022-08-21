31423925ede1aef6d22a7eadbda59768ade43641
Yagihata <yght_dnt@yahoo.co.jp>
2022-08-21 12:09:10 +0900

    パッケージ競合の回避方法を追加

--- Assets/YagihataItems/RadialInventorySystemV4/Editor/PackageInstaller.cs
+++ Assets/YagihataItems/RadialInventorySystemV4/Editor/PackageInstaller.cs
@@ -1,5 +1,7 @@
 ﻿
+using System;
 using System.Linq;
+using System.Reflection;
 using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
@@ -10,15 +12,44 @@ namespace YagihataItems.RadialInventorySystemV4
     public static class PackageInstaller
     {
         static AddRequest addRequest;
+        static RemoveRequest removeRequest;
         static ListRequest listRequest;
+
+        [MenuItem("Radial Inventory/Reload Packages")]
+        public static void ReloadPackages()
+        {
+            ScriptingDefineSymbolsUtil.Remove("RISV4_JSON");
+            removeRequest = Client.Remove("com.unity.nuget.newtonsoft-json");
+            EditorApplication.update += RemovePackage;
+        }
         [InitializeOnLoadMethod]
         static void CheckPackages()
         {
             Debug.Log("Check Package: Newtonsoft.Json");
 
-            listRequest = Client.List();
-            EditorApplication.update += UpdateList;
+            Type type = GetTypeByClassName("Newtonsoft.Json.JsonConverter");
+            if (type == null)
+            {
+                listRequest = Client.List();
+                EditorApplication.update += UpdateList;
+            }
+            else
+                ScriptingDefineSymbolsUtil.Add("RISV4_JSON");
 
+        }
+        public static Type GetTypeByClassName(string className)
+        {
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (Type type in assembly.GetTypes())
+                {
+                    if (type.FullName == className)
+                    {
+                        return type;
+                    }
+                }
+            }
+            return null;
         }
         static void AddPackage()
         {
@@ -33,6 +64,21 @@ namespace YagihataItems.RadialInventorySystemV4
                     Debug.Log(addRequest.Error.message);
 
                 EditorApplication.update -= AddPackage;
+            }
+        }
+        static void RemovePackage()
+        {
+            if (removeRequest.IsCompleted)
+            {
+                if (removeRequest.Status == StatusCode.Success)
+                {
+                    Debug.Log("Removed: " + removeRequest.PackageIdOrName);
+                    CheckPackages();
+                }
+                else if (addRequest.Status >= StatusCode.Failure)
+                    Debug.Log(addRequest.Error.message);
+
+                EditorApplication.update -= RemovePackage;
             }
         }
         static void UpdateList()
